db337cbe7570ea5243cb5f2bbf3a90e7d6036819
sasstrologer <sasstrologer@gmail.com>
2022-01-06 19:02:36 -0500

    added left mouse click to move, removed WASD functionality, increased field of view to 120 degrees

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,6 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
     public float gravity = -9.81f;
 
     Vector3 velocity;
@@ -27,14 +26,13 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
         
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            controller.SimpleMove(forward * 3);
+        }
 
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetKeyDown("space") && isGrounded)
+        if(Input.GetKey("space") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
