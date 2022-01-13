fde05b6b64942a29deabd6de1ad6b6048e8db8ac
500lbs <stevenksteffen@gmail.com>
2022-01-13 00:21:52 -0500

    updated player movement script
    ^

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,13 +11,6 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
 
-<<<<<<< Updated upstream:Assets/PlayerMovement.cs
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-    bool isGrounded;
-
-=======
      public Transform groundCheck;
      public float groundDistance = 0.4f;
      public LayerMask groundMask;
@@ -26,7 +19,6 @@ public class PlayerMovement : MonoBehaviour
      public int bugCounter = 0;
 
      public float speed = 1.25f;
->>>>>>> Stashed changes:Assets/Scripts/PlayerMovement.cs
     public float jumpHeight = 3f;
     
     void Start()
@@ -43,20 +35,13 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
         
-<<<<<<< Updated upstream:Assets/PlayerMovement.cs
-=======
-
->>>>>>> Stashed changes:Assets/Scripts/PlayerMovement.cs
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         if (Input.GetKey(KeyCode.Mouse0))
         {
             controller.SimpleMove(forward * 3);
         }
 
-<<<<<<< Updated upstream:Assets/PlayerMovement.cs
-=======
         
->>>>>>> Stashed changes:Assets/Scripts/PlayerMovement.cs
         if(Input.GetKey("space") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
@@ -65,9 +50,6 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-<<<<<<< Updated upstream:Assets/PlayerMovement.cs
-
-=======
         
     }
     
@@ -78,6 +60,5 @@ public class PlayerMovement : MonoBehaviour
             bugCounter++;
             Destroy(other.gameObject); 
         }
->>>>>>> Stashed changes:Assets/Scripts/PlayerMovement.cs
     }
 }
